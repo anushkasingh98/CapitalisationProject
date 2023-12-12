9eb39bd863ad5f5f206e88f8b9e1a11412e046b6
Anushka Singh <31279432+anushkasingh98@users.noreply.github.com>
2023-12-12 10:11:13 -0500

    Create p2.py

---
+++ p2.py
@@ -0,0 +1,17 @@
+import p1
+
+def read_text_file(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            content = file.read()
+            print(f"Content of the file '{file_path}':\n{content}")
+            return content
+    except FileNotFoundError:
+        print(f"Error: The file '{file_path}' was not found.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    file_path = "example.txt"  # Update with the actual path to your text file
+    content = read_text_file(file_path)
+    
